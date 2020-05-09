8aa0c83212f48fe1387ac5785f18a65a1040812e
furtiveJack <tanguytraina@hotmail.fr>
2020-05-09 13:07:14 +0200

    Add albumDao

--- app/src/main/java/fr/uge/soundroid/database/SoundroidDatabase.java
+++ app/src/main/java/fr/uge/soundroid/database/SoundroidDatabase.java
@@ -3,8 +3,10 @@ package fr.uge.soundroid.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Song.class}, version=1)
+@Database(entities = {Song.class, Artist.class, Album.class}, version=1)
 public abstract class SoundroidDatabase extends RoomDatabase {
     public abstract SongDao songDao();
     public abstract ArtistDao artistDao();
+    public abstract AlbumDao albumDao();
+
 }
