d0a391eb132ab5372ddac79ef49422fb23e39a04
furtiveJack <tanguytraina@hotmail.fr>
2020-05-09 13:05:57 +0200

    Update onConflictStrategy a insertion

--- app/src/main/java/fr/uge/soundroid/database/ArtistDao.java
+++ app/src/main/java/fr/uge/soundroid/database/ArtistDao.java
@@ -1,19 +1,22 @@
 package fr.uge.soundroid.database;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
 
-interface ArtistDao {
+@Dao
+public interface ArtistDao {
     @Query("SELECT * FROM artist")
     List<Artist> getAll();
 
-    @Query("SELECT * FROM artist WHERE name=:name")
+    @Query("SELECT * FROM artist WHERE artist.name=:name")
     Artist findByName(String name);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.ABORT)
     void insert(Artist artist);
 
     @Delete
