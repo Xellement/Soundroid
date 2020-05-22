23ba26f56be46595ee60bdc6f3318607d6d6c47d
furtiveJack <tanguytraina@hotmail.fr>
2020-05-22 13:42:40 +0200

    Add utils methods

--- app/src/main/java/fr/uge/soundroid/database/entity/Playlist.java
+++ app/src/main/java/fr/uge/soundroid/database/entity/Playlist.java
@@ -1,5 +1,7 @@
 package fr.uge.soundroid.database.entity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
@@ -7,8 +9,29 @@ import androidx.room.PrimaryKey;
 @Entity
 public class Playlist {
     @PrimaryKey(autoGenerate = true)
-    public int playlistId;
+    public long playlistId;
 
     @ColumnInfo(name = "name")
     public String playlistName;
+
+    public Playlist() {}
+
+    public Playlist(String name) {
+        playlistName = name;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (! (obj instanceof Playlist)) {
+            return false;
+        }
+        Playlist p = (Playlist) obj;
+        return p.playlistId == playlistId && p.playlistName.equals(playlistName);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "id: " + playlistId + " - name: " + playlistName;
+    }
 }
