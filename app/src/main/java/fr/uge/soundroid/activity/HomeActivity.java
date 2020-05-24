4765fa3851ff97286b4bcf52db518bb3d4060187
furtiveJack <tanguytraina@hotmail.fr>
2020-05-24 12:15:03 +0200

    Create start activity showing logo while waiting for indexing to finish

--- app/src/main/java/fr/uge/soundroid/activity/HomeActivity.java
+++ app/src/main/java/fr/uge/soundroid/activity/HomeActivity.java
@@ -1,16 +1,10 @@
 package fr.uge.soundroid.activity;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.ActivityManager;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -21,7 +15,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-import fr.uge.soundroid.IndexService;
 import fr.uge.soundroid.PlaylistAdapter;
 import fr.uge.soundroid.R;
 import fr.uge.soundroid.SongAdapter;
@@ -29,8 +22,6 @@ import fr.uge.soundroid.database.entity.Playlist;
 import fr.uge.soundroid.database.entity.Song;
 import fr.uge.soundroid.database.viewmodel.PlaylistViewModel;
 import fr.uge.soundroid.database.viewmodel.SongViewModel;
-
-//import fr.uge.soundroid.Playlist;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -45,7 +36,6 @@ public class HomeActivity extends AppCompatActivity {
     private PlaylistAdapter playlistAdapter = new PlaylistAdapter(songsPlaylist);
     private PlaylistAdapter favoritesAdapter;
     private SongAdapter songAdapter = new SongAdapter(musics);
-    private IndexService indexService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -129,57 +119,6 @@ public class HomeActivity extends AppCompatActivity {
         setPlaylistListenerClick(playlistAdapter);
         playlistRV.setAdapter(playlistAdapter);
         updateLayoutManager(playlistRV, 3);
-    }
-
-    private static final String[] PERMISSIONS = {
-        Manifest.permission.READ_EXTERNAL_STORAGE
-    };
-
-    private static final int REQUEST_PERMISSIONS = 1;
-
-    private static final int PERMISSIONS_COUNT = 1;
-
-    @SuppressLint("NewApi")
-    private boolean arePermissionsDenied() {
-        for (int i = 0; i < PERMISSIONS_COUNT; ++i) {
-            if (checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (arePermissionsDenied()) {
-            ((ActivityManager) (this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
-            recreate();
-        } else {
-            onResume();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionsDenied()) {
-            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
-            return;
-        }
-        // TODO : Create a Handler and use it to run indexService periodically
-        // -> we may need to store the date of last indexation in DB
-        if (indexService != null) {
-            return;
-        }
-        Thread index = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                indexService = new IndexService(HomeActivity.this.getApplication());
-                indexService.addMusicFilesFromRoot();
-            }
-        });
-        index.start();
     }
 
     private void setPlaylistListenerClick(final PlaylistAdapter adap){
---
+++ app/src/main/java/fr/uge/soundroid/activity/StartActivity.java
@@ -0,0 +1,91 @@
+package fr.uge.soundroid.activity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.uge.soundroid.IndexService;
+import fr.uge.soundroid.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    private static final int REQUEST_PERMISSIONS = 1;
+
+    private static final int PERMISSIONS_COUNT = 1;
+
+    @SuppressLint("NewApi")
+    private boolean arePermissionsDenied() {
+        for (int i = 0; i < PERMISSIONS_COUNT; ++i) {
+            if (checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (arePermissionsDenied()) {
+            ((ActivityManager) (this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+            recreate();
+        } else {
+            onResume();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionsDenied()) {
+            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+            return;
+        }
+        IndexTask index = new IndexTask(this);
+        index.execute();
+    }
+
+    private static class IndexTask extends AsyncTask<Void, Void, Void> {
+        Activity activity;
+        private IndexService indexService;
+
+        IndexTask(Activity act) {
+            activity = act;
+            indexService = new IndexService(activity.getApplication());
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            indexService.addMusicFilesFromRoot();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Intent intent = new Intent(activity.getApplicationContext(), HomeActivity.class);
+            Log.d("index", "indexing finished");
+            activity.startActivity(intent);
+            activity.finish();
+        }
+    }
+}
